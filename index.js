18334ce619e0c2a95fea665d12fe1eba5fa7feaf
lolenigmatic <quanabcis2107@gmail.com>
2019-11-13 14:27:40 +0700

    Fixed time logic again

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ function getFACEITData(callbackFunction, offsetVar, limitVar, typeVar){
 function formatDate(date) {
   var hours = date.getHours();
   var minutes = date.getMinutes();
-  var ampm = hours % 24 >= 12 ? 'pm' : 'am';
   var offset = date.getTimezoneOffset();
   var offsetHours = offset / 60;
   hours = hours + offsetHours + config.timezone; // Manipulating time so that it shows the right time zone.
+  var ampm = hours % 24 >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0'+minutes : minutes;
